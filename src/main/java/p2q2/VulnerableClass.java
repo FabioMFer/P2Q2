0eb5e85f85b5bd6f13dcec34e7cfafd5943cccd8
Fabio Martins Fernandes <fabio.martinsfernandes@gmail.com>
2017-07-06 18:24:24 -0300

    Código Sanitizado

--- src/main/java/p2q2/VulnerableClass.java
+++ src/main/java/p2q2/VulnerableClass.java
@@ -6,32 +6,40 @@ import java.io.FileReader;
 import java.io.FileWriter;
 import java.io.IOException;
 import java.util.Scanner;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 public class VulnerableClass {
+	@SuppressWarnings("resource")
 	public void vulnerableMethod(String FILENAME){
 		while (true) {
-		    Scanner console = new Scanner(System.in);
 		    System.out.print("Digite a operacao desejada para realizar no arquivo <R para ler um arquivo, "
 		    		+ "W para escrever em um arquivo>? ");
 			
-		    String opr= console.nextLine();
+		    String opr= new Scanner(System.in).nextLine();
 			
 		    if (opr.equals("R")){
-				BufferedReader br = null;
-				FileReader fr = null;
-				
+				//BufferedReader br = null;
+				//FileReader fr = null;
+				//estas variáveis tornam o código noncompliant, assim seguem as mudanças:
 				try {
-
+/*
 					fr = new FileReader(FILENAME);
 					br = new BufferedReader(fr);
 
-					String sCurrentLine;
-
 					br = new BufferedReader(new FileReader(FILENAME));
-
-					while ((sCurrentLine = br.readLine()) != null) {
+*/
+					String sCurrentLine;
+					Pattern pattern = Pattern.compile("[^A-Za-z0-9._]");
+					Matcher matcher = pattern.matcher(FILENAME);
+					if(matcher.find()){
+						System.out.println("Erro no nome do arquivo");
+					}
+					while ((sCurrentLine = new BufferedReader(new FileReader(FILENAME)).readLine()) != null) {
 						System.out.println(sCurrentLine);
 					}
+					//desta forma minimizamos os escopos das variáveis e excluimos variáveis 
+					//não utilizadas, o mesmo foi ferito com a variável console acima.
 
 				} catch (IOException e) {
 
@@ -47,7 +55,7 @@ public class VulnerableClass {
 					buffWrite = new BufferedWriter(new FileWriter(FILENAME));
 					String linha = "";
 					System.out.println("Escreva algo: ");
-				    linha = console.nextLine();
+				    linha = new Scanner(System.in).nextLine();
 				    buffWrite.append(linha + "\n");
 				     
 				} catch (IOException e) {
---
+++ src/test/java/p2q2/MaliciousTest.java
@@ -0,0 +1,14 @@
+package p2q2;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class MaliciousTest {
+
+	@Test
+	public void test() {
+		fail("Not yet implemented");
+	}
+
+}
